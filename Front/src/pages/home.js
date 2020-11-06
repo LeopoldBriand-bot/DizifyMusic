fdf6a71968a9cf89039659a06f7d4333fe962432
Leopold <leopold.briand@laposte.net>
2020-11-06 17:13:26 +0100

    feat(front): init router, home and login component

---
+++ Front/src/pages/home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import NavBar from '../component/navbar.component'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <div>
+            Home
+        </div>
+      </div>
+    )
+  }
+}export default App
---
+++ Front/src/routes.js
@@ -0,0 +1,13 @@
+import Home from './pages/home';
+import Login from './pages/login';
+
+export default [
+    {
+        path: "/login",
+        component: Login
+    },
+    {
+        path: "/home",
+        component: Home,
+    },
+  ];
