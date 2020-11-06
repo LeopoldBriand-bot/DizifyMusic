de7e0d46575e7aecaceebc2be67c61156164d5c9
Can Sesen <canishcosesen@gmail.com>
2020-11-06 15:01:18 +0100

    feat(back): rest HelloWorldController Init

---
+++ Back/src/main/java/application/DizifyApplication.java
@@ -0,0 +1,13 @@
+package application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DizifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DizifyApplication.class, args);
+    }
+
+}
