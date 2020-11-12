2c603e3436c649608d3c51d66241efa259872aca
Leopold <leopold.briand@laposte.net>
2020-11-12 09:39:44 +0100

    feat(front): init user store

---
+++ Front/src/dataHelpers/user-helper.js
@@ -0,0 +1,21 @@
+export default class UserHelper {
+    constructor() {
+
+    }
+    connect() {
+        return {}
+    }
+    disconnect() {
+        return {}
+    }
+    connect() {
+        return {}
+    }
+    connect() {
+        return {}
+    }
+    connect() {
+        return {}
+    }
+
+}
