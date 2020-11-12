ea0198b09f33266d7ec55df63f42b879dff36845
Leopold <leopold.briand@laposte.net>
2020-11-12 09:39:44 +0100

    feat(front): init user store

---
+++ Front/src/stores/user.store.js
@@ -0,0 +1,50 @@
+import React, {createContext, useContext, useReducer} from 'react';
+
+const initialState = {
+    userId: null,
+    userName: null,
+    playlists: [],
+    authToken:''
+
+};
+
+const connect = function(state, action) {
+    return {
+        ...state,
+        userId: action.userId,
+        username: action.userName,
+        playlists: action.playlists,
+        authToken: action.authToken
+    }
+}
+
+const disconnect = function(state, action) {
+    return {
+        ...state,
+        ...initialState
+    }
+}
+
+const reducer = (state, action) => {
+   switch (action.type) {
+       case 'connect':
+       return connect(state, action);
+       case 'disconnect':
+       return disconnect(state, action);
+       case 'isConnected':
+       return isTokenValid(state.authToken);
+       default:
+       return state;
+   }
+};
+
+const UserContext = createContext();
+
+export const UserConsumer = UserContext.Consumer;
+export const UserConsumerHook = () => useContext(UserContext);
+
+export const UserProvider = ({children}) => (
+   <UserContext.Provider value={useReducer(reducer, initialState)}>
+       {children}
+   </UserContext.Provider>
+);
