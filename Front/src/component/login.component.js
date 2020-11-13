858ff9181e879117e7f286d53338f2e215d428da
Baptiste LECHAT <baptiste.lechat@ynov.com>
2020-11-13 10:36:42 +0100

    feat(front): create login and signup components, manage validation

---
+++ Front/src/component/login.component.js
@@ -0,0 +1,158 @@
+import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import { Modal } from '@material-ui/core';
+import UserHelper from '../dataHelpers/user-helper';
+
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    background: '#ffffff',
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function Login(props) {
+  const classes = useStyles();
+
+  const [isPassword, setIsPassword] = React.useState(false);
+  const [isEmail, setIsEmail] = React.useState(false);
+  const [isSubmit, setIsSubmit] = React.useState(false);
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  const regexEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const regexPassword = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
+ 
+  const handleClose = () => {
+    props.setOpen(false);
+    setIsSubmit(false);
+    setIsEmail(false);
+    setIsPassword(false);
+  };
+
+  const onSubmit = () => {
+
+    setIsEmail(Boolean(regexEmail.exec(email)));
+    setIsPassword(Boolean(regexPassword.exec(password)));
+
+
+    if (Boolean(regexEmail.exec(email)) && Boolean(regexPassword.exec(password))) {
+        const helper = new UserHelper();
+        helper.connectUser({email, password});
+        props.setAuth(true);
+        handleClose();
+    } else {
+        setIsSubmit(true);
+    }
+
+  };
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+  
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const body = (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Login
+        </Typography>
+        <form className={classes.form}>
+          <TextField
+            error={isSubmit && !isEmail}
+            helperText={isSubmit && !isEmail ? ('Invalid Email - Must have format like test@test.com') : null}
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            onChange={handleEmailChange}
+          />
+          <TextField
+            error={isSubmit && !isPassword}
+            helperText={isSubmit && !isPassword ? ('Invalid Password - Must have one symbol, one uppercase, one lowercase and one number / minimum: 8 characters') : null}
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            onChange={handlePasswordChange}
+          />
+          <Button
+            onClick={onSubmit}
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item>
+              <Link href="#" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+    </Container>
+  );
+
+  return (
+    <div>
+      <Modal
+        open={props.open}
+        onClose={handleClose}
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+      >
+        {body}
+      </Modal>
+    </div>
+  );
+}
